3d88dffc4b7b839bdb440c0461e4bb38b34a1b4d
Crystal Danger <cdneal091@gmail.com>
2021-12-25 07:52:11 -0700

    update test with construction message

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders under construction message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/under construction/i);
   expect(linkElement).toBeInTheDocument();
 });
